059d5bd38a6c795295c77923bb8ea413513b7496
nengberg <niklas.engberg@creuna.se>
2015-05-04 13:37:55 +0200

    Updates profile.

--- install_scoop_and_pshazz.ps1
+++ install_scoop_and_pshazz.ps1
@@ -1,4 +1,6 @@
 # Setup Scoop with Pshazz
 
-cd .\modules\scoop\bin
-$output = .\install.ps1 -force
+iex (new-object net.webclient).downloadstring('https://get.scoop.sh')
+scoop install concfg
+scoop install pshazz
+pshazz use blouie
