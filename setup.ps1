e6fb1ce7a2338685f5ad15a0b46af22d20f09197
nengberg <per.niklas.engberg@gmail.com>
2018-11-08 11:17:22 +0100

    Added Jump.Location module to setup file

--- setup.ps1
+++ setup.ps1
@@ -1,2 +1,17 @@
-cmd /c mklink /h "C:\Program Files\ConEmu\ConEmu.xml" "C:\powershell-profile\ConEmu.xml"
-cmd /c mklink /h $PROFILE "C:\powershell-profile\Microsoft.PowerShell_profile.ps1"
+function Install($module) {
+    if (Get-Module -ListAvailable -Name $module) {
+        Write-Host "$module already installed. Not installing."
+    } else {
+        Write-Host "$module not installed. Installing"
+        Install-Module $module
+    }
+}
+
+function SymLink($link, $target) {
+    cmd /c mklink /h $link $target
+}
+
+Install "Jump.Location"
+
+SymLink "C:\Program Files\ConEmu\ConEmu.xml" "C:\powershell-profile\ConEmu.xml"
+SymLink $PROFILE "C:\powershell-profile\Microsoft.PowerShell_profile.ps1"
